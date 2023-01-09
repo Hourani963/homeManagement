f11e4852a406d774e6ae7f5c298f662f6669183d
AL HARIRI <ahmdhrere@gmail.com>
2023-01-09 23:02:07 +0100

    Docker-compose works

--- homeManagementFront/src/app/apiUrls.ts
+++ homeManagementFront/src/app/apiUrls.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "http://127.0.0.1:8080";
 
-export const articleUrl = "http://localhost:8080/article";
+export const articleUrl = "http://127.0.0.1:8080/article";
 
-export const categoryUrl = "http://localhost:8080/category"
+export const categoryUrl = "http://127.0.0.1:8080/category"
